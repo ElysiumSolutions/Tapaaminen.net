7f534af0da95b6d9cf3cedb7d92c2a9530e84970
Marko Kaartinen <marko@elysium.fi>
2017-07-18 17:43:47 +0300

    Korjaa ongelman kunnes päivitetään mariadb 10.2
    Fixes #2
    Fixes #3
    
    https://laravel-news.com/laravel-5-4-key-too-long-error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Contracts\Logging\Log;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Validator;
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+    	//fix until upgraded to mariadb 10.2
+	    Schema::defaultStringLength(191);
+
         Carbon::setLocale('fi');
 
         Validator::extend('checkHashed', function($attribute, $value, $parameters)
