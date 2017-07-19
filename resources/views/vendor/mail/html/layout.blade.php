76c92bd2c9618c7251657ae8ccad724e35d5eabc
Marko Kaartinen <marko@elysium.fi>
2017-07-19 08:08:31 +0300

    Sähköpostin ulkoasua

--- resources/views/vendor/mail/html/layout.blade.php
+++ resources/views/vendor/mail/html/layout.blade.php
@@ -39,6 +39,8 @@
                                         {{ Illuminate\Mail\Markdown::parse($slot) }}
 
                                         {{ $subcopy or '' }}
+
+                                        <small><i>Ethän merkitse näitä sähköposteja roskapostiksi. Mikäli koet, että näitä tulee turhaan voit olla yhteydessä apua@tapaaminen.net osoitteeseen!</i></small>
                                     </td>
                                 </tr>
                             </table>
