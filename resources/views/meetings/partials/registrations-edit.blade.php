c09a94fffe61ef71607f7715bca1ab0e0e521f01
Marko Kaartinen <marko@elysium.fi>
2017-08-25 09:30:22 +0300

    Sivuttain skrollauksen korjaus jos paljon aikoja
    Fixes #23

--- resources/views/meetings/partials/registrations-edit.blade.php
+++ resources/views/meetings/partials/registrations-edit.blade.php
@@ -4,101 +4,103 @@
         {{ csrf_field() }}
         {{ method_field('delete') }}
 
-        <table class="table timestable">
-            <thead>
-            <tr>
-                <td class="empty">&nbsp;</td>
-                @foreach($times as $month => $days)
-                    <td class="month" colspan="{{ $amounts[$month] }}">{{ $meeting->formatMonth($month) }}</td>
-                @endforeach
-                <td class="empty">&nbsp;</td>
-            </tr>
-            <tr>
-                <td class="empty">&nbsp;</td>
-                @foreach($times as $month => $days)
-                    @foreach($days as $day => $times2)
-                        <td class="day" colspan="{{ count($times2) }}">{{ $meeting->formatDay($day, $month) }}</td>
+        <div id="timestable-wrapper">
+            <table class="table timestable">
+                <thead>
+                <tr>
+                    <td class="empty">&nbsp;</td>
+                    @foreach($times as $month => $days)
+                        <td class="month" colspan="{{ $amounts[$month] }}">{{ $meeting->formatMonth($month) }}</td>
                     @endforeach
-                @endforeach
-                <td class="empty">&nbsp;</td>
-            </tr>
-            <tr>
-                <td class="empty" style="text-align: right;">{{ count($meeting->registrations) }} ilmoittautumista</td>
-                @foreach($times as $month => $days)
-                    @foreach($days as $day => $times2)
-                        @foreach($times2 as $time)
-                            <td class="time">{{ $time['time'] }}</td>
+                    <td class="empty">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="empty">&nbsp;</td>
+                    @foreach($times as $month => $days)
+                        @foreach($days as $day => $times2)
+                            <td class="day" colspan="{{ count($times2) }}">{{ $meeting->formatDay($day, $month) }}</td>
                         @endforeach
                     @endforeach
-                @endforeach
-                <td class="empty">&nbsp;</td>
-            </tr>
-            </thead>
-            <tbody>
-			<?php $timeamounts = array(); ?>
-
-            @foreach($meeting->registrations as $registration)
-                <tr class="register">
-                    <td class="username">
-                                <span class="icon is-small is-left">
-                                    <i class="fa fa-user"></i>
-                                </span>
-                        {{ $registration->username }}
-                    </td>
+                    <td class="empty">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="empty" style="text-align: right;">{{ count($meeting->registrations) }} ilmoittautumista</td>
                     @foreach($times as $month => $days)
                         @foreach($days as $day => $times2)
                             @foreach($times2 as $time)
-                                @if(in_array($time['id'], json_decode($registration->times)))
-
-									<?php
-									if( ! isset( $timeamounts[ $time['id'] ] ) ) {
-										$timeamounts[ $time['id'] ] = 0;
-									}
-									$timeamounts[$time['id']]++;
-									?>
-
-                                    <td class="yes">
-                                                <span class="icon is-small">
-                                                    <i class="fa fa-check"></i>
-                                                </span>
-                                    </td>
-                                @else
-                                    <td class="no">&nbsp;</td>
-                                @endif
+                                <td class="time">{{ $time['time'] }}</td>
                             @endforeach
                         @endforeach
                     @endforeach
-                    <td>
-                        <button name="registration" value="{{ $registration->id }}" class="button is-danger is-small is-outlined">
-                            <span class="icon is-small">
-                                <i class="fa fa-times"></i>
-                            </span>
-                            <span>Poista</span>
-                        </button>
-                    </td>
+                    <td class="empty">&nbsp;</td>
                 </tr>
-            @endforeach
-            </tbody>
-            <tfoot>
-            <tr>
-                <td class="empty">&nbsp;</td>
-				<?php $k = 0; ?>
-                @foreach($times as $month => $days)
-                    @foreach($days as $day => $times2)
-                        @foreach($times2 as $time)
-                            <td class="amount">
-                                @if(isset($timeamounts[$time['id']]))
-                                    {{ $timeamounts[$time['id']] }}
-                                @else
-                                    0
-                                @endif
-                            </td>
-							<?php $k++; ?>
+                </thead>
+                <tbody>
+                <?php $timeamounts = array(); ?>
+
+                @foreach($meeting->registrations as $registration)
+                    <tr class="register">
+                        <td class="username">
+                                    <span class="icon is-small is-left">
+                                        <i class="fa fa-user"></i>
+                                    </span>
+                            {{ $registration->username }}
+                        </td>
+                        @foreach($times as $month => $days)
+                            @foreach($days as $day => $times2)
+                                @foreach($times2 as $time)
+                                    @if(in_array($time['id'], json_decode($registration->times)))
+
+                                        <?php
+                                        if( ! isset( $timeamounts[ $time['id'] ] ) ) {
+                                            $timeamounts[ $time['id'] ] = 0;
+                                        }
+                                        $timeamounts[$time['id']]++;
+                                        ?>
+
+                                        <td class="yes">
+                                                    <span class="icon is-small">
+                                                        <i class="fa fa-check"></i>
+                                                    </span>
+                                        </td>
+                                    @else
+                                        <td class="no">&nbsp;</td>
+                                    @endif
+                                @endforeach
+                            @endforeach
+                        @endforeach
+                        <td>
+                            <button name="registration" value="{{ $registration->id }}" class="button is-danger is-small is-outlined">
+                                <span class="icon is-small">
+                                    <i class="fa fa-times"></i>
+                                </span>
+                                <span>Poista</span>
+                            </button>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+                <tfoot>
+                <tr>
+                    <td class="empty">&nbsp;</td>
+                    <?php $k = 0; ?>
+                    @foreach($times as $month => $days)
+                        @foreach($days as $day => $times2)
+                            @foreach($times2 as $time)
+                                <td class="amount">
+                                    @if(isset($timeamounts[$time['id']]))
+                                        {{ $timeamounts[$time['id']] }}
+                                    @else
+                                        0
+                                    @endif
+                                </td>
+                                <?php $k++; ?>
+                            @endforeach
                         @endforeach
                     @endforeach
-                @endforeach
-            </tr>
-            </tfoot>
-        </table>
+                </tr>
+                </tfoot>
+            </table>
+        </div>
     </form>
 </div>
--- resources/views/meetings/partials/registrations-show.blade.php
+++ resources/views/meetings/partials/registrations-show.blade.php
@@ -2,131 +2,134 @@
     <h3 class="title is-4">Milloin sinulle sopisi?</h3>
     <form role="form" method="POST" action="{{ url('/s/'.$meeting->slug.'/times') }}">
         {{ csrf_field() }}
-        <table class="table timestable">
-            <thead>
-            <tr>
-                <td class="empty">&nbsp;</td>
-                @foreach($times as $month => $days)
-                    <td class="month" colspan="{{ $amounts[$month] }}">{{ $meeting->formatMonth($month) }}</td>
-                @endforeach
-            </tr>
-            <tr>
-                <td class="empty">&nbsp;</td>
-                @foreach($times as $month => $days)
-                    @foreach($days as $day => $times2)
-                        <td class="day" colspan="{{ count($times2) }}">{{ $meeting->formatDay($day, $month) }}</td>
+
+        <div id="timestable-wrapper">
+            <table class="table timestable">
+                <thead>
+                <tr>
+                    <td class="empty">&nbsp;</td>
+                    @foreach($times as $month => $days)
+                        <td class="month" colspan="{{ $amounts[$month] }}">{{ $meeting->formatMonth($month) }}</td>
                     @endforeach
-                @endforeach
-            </tr>
-            <tr>
-                <td class="empty" style="text-align: right;">{{ count($meeting->registrations) }} ilmoittautumista</td>
-                @foreach($times as $month => $days)
-                    @foreach($days as $day => $times2)
-                        @foreach($times2 as $time)
-                            <td class="time">{{ $time['time'] }}</td>
+                </tr>
+                <tr>
+                    <td class="empty">&nbsp;</td>
+                    @foreach($times as $month => $days)
+                        @foreach($days as $day => $times2)
+                            <td class="day" colspan="{{ count($times2) }}">{{ $meeting->formatDay($day, $month) }}</td>
                         @endforeach
                     @endforeach
-                @endforeach
-            </tr>
-            </thead>
-            <tbody>
-			<?php $timeamounts = array(); ?>
-
-            @foreach($meeting->registrations as $registration)
-                <tr class="register">
-                    <td class="username">
-                                <span class="icon is-small is-left">
-                                    <i class="fa fa-user"></i>
-                                </span>
-                        @if($meeting->settings->shownames)
-                            {{ $registration->username }}
-                        @else
-                            <i>Piilotettu</i>
-                        @endif
-                    </td>
+                </tr>
+                <tr>
+                    <td class="empty" style="text-align: right;">{{ count($meeting->registrations) }} ilmoittautumista</td>
                     @foreach($times as $month => $days)
                         @foreach($days as $day => $times2)
                             @foreach($times2 as $time)
-                                @if(in_array($time['id'], json_decode($registration->times)))
-
-									<?php
-									if( ! isset( $timeamounts[ $time['id'] ] ) ) {
-										$timeamounts[ $time['id'] ] = 0;
-									}
-									$timeamounts[$time['id']]++;
-									?>
-
-                                    <td class="yes">
-                                                <span class="icon is-small">
-                                                    <i class="fa fa-check"></i>
-                                                </span>
-                                    </td>
-                                @else
-                                    <td class="no">&nbsp;</td>
-                                @endif
+                                <td class="time">{{ $time['time'] }}</td>
                             @endforeach
                         @endforeach
                     @endforeach
                 </tr>
-            @endforeach
-            </tbody>
-            <tfoot>
-            <tr class="registration">
-                <td>
-                    <div class="field">
-                        <p class="control has-icons-left has-icons-right">
-							<?php if(Auth::Check()){ $defaultname = Auth::User()->name; }else{ $defaultname = ""; } ?>
+                </thead>
+                <tbody>
+                <?php $timeamounts = array(); ?>
 
-                            <input class="input is-small" type="text" name="name" placeholder="Nimesi" value="{{ old('name', $defaultname) }}">
-                            <span class="icon is-small is-left">
+                @foreach($meeting->registrations as $registration)
+                    <tr class="register">
+                        <td class="username">
+                                    <span class="icon is-small is-left">
                                         <i class="fa fa-user"></i>
                                     </span>
-                        </p>
-                    </div>
-                </td>
-                @foreach($times as $month => $days)
-                    @foreach($days as $day => $times2)
-                        @foreach($times2 as $time)
-                            <td class="timecell">
-                                <div class="field">
-                                    <p class="control">
-                                        <label class="checkbox">
-                                            <input type="checkbox" name="times[]" value="{{ $time['id'] }}">
-                                        </label>
-                                    </p>
-                                </div>
-                            </td>
+                            @if($meeting->settings->shownames)
+                                {{ $registration->username }}
+                            @else
+                                <i>Piilotettu</i>
+                            @endif
+                        </td>
+                        @foreach($times as $month => $days)
+                            @foreach($days as $day => $times2)
+                                @foreach($times2 as $time)
+                                    @if(in_array($time['id'], json_decode($registration->times)))
+
+                                        <?php
+                                        if( ! isset( $timeamounts[ $time['id'] ] ) ) {
+                                            $timeamounts[ $time['id'] ] = 0;
+                                        }
+                                        $timeamounts[$time['id']]++;
+                                        ?>
+
+                                        <td class="yes">
+                                                    <span class="icon is-small">
+                                                        <i class="fa fa-check"></i>
+                                                    </span>
+                                        </td>
+                                    @else
+                                        <td class="no">&nbsp;</td>
+                                    @endif
+                                @endforeach
+                            @endforeach
+                        @endforeach
+                    </tr>
+                @endforeach
+                </tbody>
+                <tfoot>
+                <tr class="registration">
+                    <td>
+                        <div class="field">
+                            <p class="control has-icons-left has-icons-right">
+                                <?php if(Auth::Check()){ $defaultname = Auth::User()->name; }else{ $defaultname = ""; } ?>
+
+                                <input class="input is-small" type="text" name="name" placeholder="Nimesi" value="{{ old('name', $defaultname) }}">
+                                <span class="icon is-small is-left">
+                                            <i class="fa fa-user"></i>
+                                        </span>
+                            </p>
+                        </div>
+                    </td>
+                    @foreach($times as $month => $days)
+                        @foreach($days as $day => $times2)
+                            @foreach($times2 as $time)
+                                <td class="timecell">
+                                    <div class="field">
+                                        <p class="control">
+                                            <label class="checkbox">
+                                                <input type="checkbox" name="times[]" value="{{ $time['id'] }}">
+                                            </label>
+                                        </p>
+                                    </div>
+                                </td>
+                            @endforeach
                         @endforeach
                     @endforeach
-                @endforeach
-            </tr>
-            <tr>
-                <td class="empty">&nbsp;</td>
-				<?php $k = 0; ?>
-                @foreach($times as $month => $days)
-                    @foreach($days as $day => $times2)
-                        @foreach($times2 as $time)
-                            <td class="amount">
-                                @if(isset($timeamounts[$time['id']]))
-                                    {{ $timeamounts[$time['id']] }}
-                                @else
-                                    0
-                                @endif
-                            </td>
-							<?php $k++; ?>
+                </tr>
+                <tr>
+                    <td class="empty">&nbsp;</td>
+                    <?php $k = 0; ?>
+                    @foreach($times as $month => $days)
+                        @foreach($days as $day => $times2)
+                            @foreach($times2 as $time)
+                                <td class="amount">
+                                    @if(isset($timeamounts[$time['id']]))
+                                        {{ $timeamounts[$time['id']] }}
+                                    @else
+                                        0
+                                    @endif
+                                </td>
+                                <?php $k++; ?>
+                            @endforeach
                         @endforeach
                     @endforeach
-                @endforeach
-            </tr>
-            <tr>
-                <td class="empty">&nbsp;</td>
-                <td class="registerbutton" colspan="{{ $k }}">
-                    <button type="submit" class="button is-success is-small">ILMOITTAUDU</button>
-                    <br /><br />
-                    @include('layouts.errors')
-                </td>
-            </tr>
-            </tfoot>
-        </table>
+                </tr>
+                <tr>
+                    <td class="empty">&nbsp;</td>
+                    <td class="registerbutton" colspan="{{ $k }}">
+                        <button type="submit" class="button is-success is-small">ILMOITTAUDU</button>
+                        <br /><br />
+                        @include('layouts.errors')
+                    </td>
+                </tr>
+                </tfoot>
+            </table>
+        </div>
     </form>
 </div>
